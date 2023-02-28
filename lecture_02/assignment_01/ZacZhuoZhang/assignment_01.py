43e30c841231b24c1918b68a95377cd14c0b6d85
Zac Zhang <zac.z.zhang@outlook.com>
2023-02-28 15:14:37 +0100

    modified:   lecture_02/assignment_01/ZacZhuoZhang/assignment_01.py

--- lecture_02/assignment_01/ZacZhuoZhang/assignment_01.py
+++ lecture_02/assignment_01/ZacZhuoZhang/assignment_01.py
@@ -1,3 +1,5 @@
+import math
+
 from compas.artists import Artist
 from compas.datastructures import Mesh
 from compas.geometry import Box
@@ -5,17 +7,16 @@ from compas.geometry import Frame
 from compas.geometry import Plane
 from compas.geometry import Projection
 from compas.geometry import Vector
-import math
 
 # Define a Frame, which is not in the origin and a bit tilted to the world frame
-frame = Frame([1, 1, 1], [1, 1, 0], [0,1, 1])
+frame = Frame([1, 1, 1], [1, 1, 0], [0, 1, 1])
 
 # Create a Box with that frame
 box = Box(frame, 1, 1, 1)
 outTemp = box
 
 # Create a Projection (can be orthogonal, parallel or perspective)
-P  = Projection.from_plane_and_direction(Plane.worldXY(), Vector(math.sin(θ)*math.cos(φ), math.sin(θ)*math.sin(φ),math.cos(θ)))
+P  = Projection.from_plane_and_direction(Plane.worldXY(), Vector(math.sin(a)*math.cos(b), math.sin(a)*math.sin(b),math.cos(a)))
 
 # Create a Mesh from the Box
 mesh = Mesh.from_shape(box)
@@ -29,5 +30,6 @@ artist1 = Artist(box)
 artist2 = Artist(mesh_projected)
 
 # Draw and all to a list
-# artist1.draw()
-# artist2.draw_edges(color="#00ff00")
+x =artist1.draw()
+y= artist2.draw_edges(color="#00ff00")
+
