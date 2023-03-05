a959e912c6998843827e5a08f00395b0574b37a5
issuehung <113417025+issuehung@users.noreply.github.com>
2023-03-05 11:05:01 +0100

    a01-yihsiuhung

---
+++ lecture_02/assignment_01/yihsiu_hung/assignment_01.py
@@ -0,0 +1,33 @@
+"""Assignment 01: Project box to xy-plane
+"""
+from compas.artists import Artist
+from compas.datastructures import Mesh
+from compas.geometry import Box
+from compas.geometry import Frame
+from compas.geometry import Plane
+from compas.geometry import Projection
+
+# Define a Frame, which is not in the origin and a bit tilted to the world frame
+frame = Frame([0, 0, 0], [3, 0, 0], [0, 2, 0])
+
+# Create a Box with that frame
+width, length, height = 1, 1, 1
+box = Box(frame, width, length, height)
+
+# Create a Projection (can be orthogonal, parallel or perspective)
+plane = Plane([0, 0, 0], [0, 0, 1])
+P = Projection.from_plane(plane)
+
+# Create a Mesh from the Box
+mesh = Mesh.from_shape(box)
+
+# Apply the Projection onto the mesh
+mesh_projected = mesh.transformed(P)
+
+# Create artists
+artist1 = Artist(box)
+artist2 = Artist(mesh_projected)
+
+# Draw and all to a list
+artist1.draw()
+artist2.draw_edges(color="#00ff00")
