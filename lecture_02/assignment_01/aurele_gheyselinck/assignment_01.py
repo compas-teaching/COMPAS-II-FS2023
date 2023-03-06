840b5b2ab8d39d7e5c5afc173189e5b8cd73cacb
gheyselinck <gheyselinck@arch.ethz.ch>
2023-03-06 11:31:20 +0100

    Submission a01
    Submission of the Assignment 01

--- lecture_02/assignment_01/aurele_gheyselinck/assignment_01.py
+++ lecture_02/assignment_01/aurele_gheyselinck/assignment_01.py
@@ -8,19 +8,24 @@ from compas.geometry import Plane
 from compas.geometry import Projection
 
 # Define a Frame, which is not in the origin and a bit tilted to the world frame
-# frame =
+frame = Frame([12, 33, 61], [-.3, 1, .6], [0.5, 0.3, 1])
+
+# Dimension variables of the box
+xsize = 12
+ysize = 33
+zsize = 61
 
 # Create a Box with that frame
-# box = ...
+box = Box(frame, xsize, ysize, zsize)
 
 # Create a Projection (can be orthogonal, parallel or perspective)
-# P = Projection.from_...
+P = Projection.from_plane(Plane.worldXY())
 
 # Create a Mesh from the Box
 mesh = Mesh.from_shape(box)
 
 # Apply the Projection onto the mesh
-# mesh_projected = ...
+mesh_projected = mesh.transformed(P)
 
 # Create artists
 artist1 = Artist(box)
