db0b622ec5cdaf6f52925d472a0f97978b7e5673
gheyselinck <gheyselinck@arch.ethz.ch>
2023-03-10 18:48:58 +0100

    some test on the geometries

--- lecture_03/assignment_02/gheyselinck_aurele/assignment_02.py
+++ lecture_03/assignment_02/gheyselinck_aurele/assignment_02.py
@@ -4,6 +4,7 @@ from compas.geometry import Circle
 from compas.geometry import Cylinder
 from compas.geometry import Frame
 from compas.geometry import Plane
+from compas.geometry import Box
 from compas.geometry import Translation
 from compas.robots import Configuration
 from compas.robots import Joint
@@ -14,30 +15,41 @@ radius, length = 0.3, 5
 cylinder = Cylinder(Circle(Plane([0, 0, 0], [1, 0, 0]), radius), length)
 cylinder.transform(Translation.from_vector([length / 2.0, 0, 0]))
 
+# create a box for the third joint
+box = Box(Frame.worldYZ(), 0.5, 0.5, 20.0)
+box.transform(Translation.from_vector([10, 0, 0]))
+#box_artist = Artist(box)
+#box_artist.draw()
+
 # create robot model
 model = RobotModel("robot", links=[], joints=[])
 
 # link meshes (calling Mesh.from_shape effectively creates a copy of the shape)
 mesh1 = Mesh.from_shape(cylinder)
-mesh2 = Mesh.from_shape(cylinder)
+mesh2 = Mesh.from_shape(box)
+mesh3 = Mesh.from_shape(cylinder)
 
 # add links
 link0 = model.add_link("world")
 link1 = model.add_link("link1", visual_mesh=mesh1, visual_color=(0.2, 0.5, 0.6))
 link2 = model.add_link("link2", visual_mesh=mesh2, visual_color=(0.5, 0.6, 0.2))
+link3 = model.add_link("link3", visual_mesh=mesh3, visual_color=(1.0 ,1.0 ,1.0))
 
 # add joints between the links
 axis = (0, 0, 1)
 
-origin = Frame.worldXY()
-model.add_joint("joint1", Joint.CONTINUOUS, link0, link1, origin, axis)
+origin1 = Frame.worldXY()
+model.add_joint("joint1", Joint.CONTINUOUS, link0, link1, origin1, axis)
 
-origin = Frame((length, 0, 0), (1, 0, 0), (0, 1, 0))
-model.add_joint("joint2", Joint.CONTINUOUS, link1, link2, origin, axis)
+origin2 = Frame((length, 0, 0), (1, 0, 0), (0, 1, 0))
+model.add_joint("joint2", Joint.CONTINUOUS, link1, link2, origin2, axis)
+
+origin3 = Frame((length+20, 0, 0), (1, 0, 0), (0, 1, 0))
+model.add_joint("joint3", Joint.CONTINUOUS, link2, link3, origin3, (1, 0, 0))
 
 # Create a configuration object matching the number of joints in your model
 configuration = model.zero_configuration()
-configuration.joint_values = [3.14, 1.57]
+configuration.joint_values = [1,1,1]
 
 # Update the model using the artist
 artist = Artist(model)
@@ -45,4 +57,4 @@ artist.update(configuration)
 
 # Render everything
 artist.draw_visual()
-artist.redraw()
+artist.redraw
