690f753b51a212d69132c2517afa1c7de7d1847c
Ahmed Elmaraghy <31185492+ahmedelmaraghy@users.noreply.github.com>
2023-03-12 22:52:22 +0100

    update

--- lecture_03/201_visualize_model_artist.py
+++ lecture_03/201_visualize_model_artist.py
@@ -1,9 +1,15 @@
-from compas.artists import Artist
+from compas.robots import GithubPackageMeshLoader
 from compas.robots import RobotModel
 
-model = RobotModel.from_urdf_file("models/01_myfirst.urdf")
+# Select Github repository, package and branch where the model is stored
+r = "ros-industrial/abb"
+p = "abb_irb6600_support"
+b = "kinetic-devel"
 
-artist = Artist(model, layer="Robot")
-artist.clear_layer()
-artist.draw_visual()
-artist.redraw()
+github = GithubPackageMeshLoader(r, p, b)
+urdf = github.load_urdf("irb6640.urdf")
+
+# Create robot model from URDF
+model = RobotModel.from_urdf_file(urdf)
+
+print(model)
