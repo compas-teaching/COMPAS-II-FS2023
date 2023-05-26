1ca554886b4c749542febecd242ada3b4d81995c
Brian Chen <yechen@student.ethz.ch>
2023-05-26 11:41:34 +0200

    commit

--- lecture_09/3_your_own_function/my_slicing_texture.py
+++ lecture_09/3_your_own_function/my_slicing_texture.py
@@ -2,20 +2,23 @@ from compas_slicer.geometry import Path
 from compas_slicer.utilities.utils import get_normal_of_path_on_xy_plane
 from compas.geometry import Point
 from compas.geometry import scale_vector, add_vectors
+from compas.geometry import rotate_points
+import math
 
 def create_overhang_texture(slicer, overhang_distance):
     print("create_overhang_texture")
 
-
+    angle = 0
+    increment = math.radians(7)
 
     for i,layer in enumerate(slicer.layers):
-        if i%3 == 0 and i>0:
+        if i% 3 == 0 and i>0:
             for j,path in enumerate(layer.paths):
 
                 new_path_f = []
                 for k , pt in enumerate(path.points):
 
-                    if k %4 ==0:
+                    if k %7 ==0:
                         normal = get_normal_of_path_on_xy_plane(k,pt,path, mesh=None)
                         normal_scaled = scale_vector(normal, -overhang_distance)
                         new_pt = add_vectors(pt,normal_scaled)
@@ -25,23 +28,34 @@ def create_overhang_texture(slicer, overhang_distance):
                     else:
                         new_path_f.append(pt)
 
-                layer.paths[j] = Path(new_path_f, is_closed=path.is_closed)
+                # lets rotate all points
+                rotated_points = rotate_points(new_path_f, angle, [0,0,1], [0,0,0])
+                rotated_points = [Point(*p) for p in rotated_points]
 
-        else:
-            i%3 != 0 and i>0
-            for j,path in enumerate(layer.paths):
+                # compas_rotated_points = []
+                # for p in rotated_points:
+                #     compas_rotated_points.append(Point(*p))
+                # rotated_points = compas_rotated_points
 
-                new_path_s = []
-                for k , pt in enumerate(path.points):
-                    if k %2 ==0:
-                        normal = get_normal_of_path_on_xy_plane(k,pt,path, mesh=None)
-                        normal_scaled = scale_vector(normal, -15)
-                        new_pt = add_vectors(pt,normal_scaled)
+                angle += increment 
 
-                        new_compas_pt = Point(new_pt[0], new_pt[1], new_pt[2])
-                        new_path_s.append(new_compas_pt)
-                    else:
-                        new_path_s.append(pt)
+                layer.paths[j] = Path(rotated_points, is_closed=path.is_closed)
 
-                layer.paths[j] = Path(new_path_s, is_closed=path.is_closed)
+        # else:
+        #     i%3 != 0 and i>0
+        #     for j,path in enumerate(layer.paths):
+
+        #         new_path_s = []
+        #         for k , pt in enumerate(path.points):
+        #             if k %2 ==0:
+        #                 normal = get_normal_of_path_on_xy_plane(k,pt,path, mesh=None)
+        #                 normal_scaled = scale_vector(normal, -15)
+        #                 new_pt = add_vectors(pt,normal_scaled)
+
+        #                 new_compas_pt = Point(new_pt[0], new_pt[1], new_pt[2])
+        #                 new_path_s.append(new_compas_pt)
+        #             else:
+        #                 new_path_s.append(pt)
+
+        #         layer.paths[j] = Path(new_path_s, is_closed=path.is_closed)
                 
