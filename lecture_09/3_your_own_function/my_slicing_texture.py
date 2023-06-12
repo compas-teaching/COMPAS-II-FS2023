2cf4a8161f7952d2091450956d76762423e9c293
alpittiglio <alexandra.pittiglio@gmail.com>
2023-06-13 00:41:43 +0200

    a04 update

--- lecture_09/3_your_own_function/my_slicing_texture.py
+++ lecture_09/3_your_own_function/my_slicing_texture.py
@@ -4,4 +4,22 @@ from compas.geometry import Point
 from compas.geometry import scale_vector, add_vectors
 
 def create_overhang_texture(slicer, overhang_distance):
-    ...
+    print("Creating overhang texture")
+
+    for i, layer in enumerate(slicer.layers):
+        if i%5 == 0 and i >0:
+            #only take every 5th layer and ignore first layer
+            for j, path in enumerate(layer.paths):
+                new_path = []
+                for k, pt in enumerate(path.points):
+                    if k%2 ==0:
+                        #only take every 2nd point
+                        normal = get_normal_of_path_on_xy_plane(k, pt, path, mesh = None)
+                        normal_scaled = scale_vector(normal, -overhang_distance)
+                        new_pt = add_vectors(pt, normal_scaled)
+                        new_compas_pt = Point(new_pt[0], new_pt[1], new_pt[2])      
+                        new_path.append(new_compas_pt)
+                    else:
+                        new_path.append(pt)
+                
+                layer.paths[j] = Path(new_path, is_closed=path.is_closed)
